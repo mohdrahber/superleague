af2a8dc0971ff5c6330117c457f29b4117e63ec8
root <mohdrahbermbd@gmail.com>
2022-02-15 06:22:45 +0000

    index.html

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+        <h1 class="heading">Super Over League</h1>
+      <div>
+        <img
+          class="image"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          class="image"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+);
+
+ReactDOM.render(element, document.getElementById('root'))
