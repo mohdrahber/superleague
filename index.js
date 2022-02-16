231bc28be6434db78e4aa35fe57b2ad9f0f4b671
MOHD RAHBER <67962735+mohdrahber@users.noreply.github.com>
2022-02-16 11:44:06 +0530

    Wrap into single jsx Element

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const element = (
-        <h1 className = "heading">Super Over League</h1>
-      <div>
-        <img
-          className="image"
-          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
-        />
-        <img
-          className="image"
-          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
-        />
-      </div>
+        <div>
+                <h1 className = "heading">Super Over League</h1>
+              <div>
+                <img
+                  className="image"
+                  src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+                />
+                <img
+                  className="image"
+                  src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+                />
+              </div>
+        </div>
 );
 
 ReactDOM.render(element, document.getElementById('root'))
